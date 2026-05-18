1fb9f634cf55725ba5a01e42f2c12cb57a7d89d8
Ali Idrees <aliidrees394@gmail.com>
2026-05-18 21:53:29 +0100

    started on orch

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-def main():
-    print("Hello from research-agents!")
-
-
-if __name__ == "__main__":
-    main()
+from src.agents.quality_control.review_agent import build_review_agent
+from src.agents.quality_control.revision_agent import build_revision_agent
+from src.agents.planner_agent import build_planner_agent
+from src.agents.search_agent import build_search_agent
+from src.agents.writer_agent import build_writer_agent
---
+++ src/orchestrator.py
@@ -0,0 +1,10 @@
+class EssayOrchestrator:
+
+    def __init__(self):
+        self.plannerAgent
+        self.searchAgent
+        self.writerAgent
+        self.reviewAgent
+        self.revisionAgent
+
+    
